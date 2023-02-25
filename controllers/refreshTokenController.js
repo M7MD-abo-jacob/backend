46fd185153eb8d47ca9068ca9b9f60d4e01d420b
M7MD-abo-jacob <105122190+M7MD-abo-jacob@users.noreply.github.com>
2023-02-25 17:29:59 +0300

    Update refreshTokenController.js

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -23,7 +23,7 @@ const handleRefreshToken = async (req, res) => {
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "10s" }
     );
-    res.json({ accessToken });
+    res.json({ "username": foundUser.username, "accessToken": accessToken });
   });
 };
 
